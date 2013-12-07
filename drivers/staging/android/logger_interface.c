ef01531dbe115a34573cdf79adbe4504338358c4
arter97 <qkrwngud825@gmail.com>
2013-12-07 13:13:04 +0900

    logger-interface: fix whitespaces

--- drivers/staging/android/logger_interface.c
+++ drivers/staging/android/logger_interface.c
@@ -12,20 +12,16 @@
  *
  */
 
-
 #include <linux/kobject.h>
 #include <linux/sysfs.h>
 #include "logger_interface.h"
 
-
 int logger_mode;
-
 
 /* sysfs interface for logger mode */
 
 static ssize_t logger_mode_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
-
 	// print current mode
 	if (logger_mode == 0)
 	{
@@ -35,13 +31,10 @@ static ssize_t logger_mode_show(struct kobject *kobj, struct kobj_attribute *att
 	{
 		return sprintf(buf, "logger mode: %d (enabled)", logger_mode);
 	}
-
 }
-
 
 static ssize_t logger_mode_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
 {
-
 	unsigned int ret = -EINVAL;
 	int val;
 
@@ -49,7 +42,7 @@ static ssize_t logger_mode_store(struct kobject *kobj, struct kobj_attribute *at
 	ret = sscanf(buf, "%d", &val);
 
 	// check value and store if valid
-	if ((val == 0) ||  (val == 1))
+	if ((val == 0) || (val == 1))
 	{
 		logger_mode = val;
 	}
@@ -57,19 +50,18 @@ static ssize_t logger_mode_store(struct kobject *kobj, struct kobj_attribute *at
 	return count;
 }
 
-
 /* Initialize logger_mode sysfs folder */
 
 static struct kobj_attribute logger_mode_attribute =
-__ATTR(logger_mode, 0666, logger_mode_show, logger_mode_store);
+	__ATTR(logger_mode, 0666, logger_mode_show, logger_mode_store);
 
 static struct attribute *logger_mode_attrs[] = {
-&logger_mode_attribute.attr,
-NULL,
+	&logger_mode_attribute.attr,
+	NULL,
 };
 
 static struct attribute_group logger_mode_attr_group = {
-.attrs = logger_mode_attrs,
+	.attrs = logger_mode_attrs,
 };
 
 static struct kobject *logger_mode_kobj;
@@ -78,23 +70,23 @@ int logger_mode_init(void)
 {
 	int logger_mode_retval;
 
-        logger_mode_kobj = kobject_create_and_add("logger_mode", kernel_kobj);
+	logger_mode_kobj = kobject_create_and_add("logger_mode", kernel_kobj);
 
-        if (!logger_mode_kobj) {
-                return -ENOMEM;
-        }
+	if (!logger_mode_kobj) {
+		return -ENOMEM;
+	}
 
-        logger_mode_retval = sysfs_create_group(logger_mode_kobj, &logger_mode_attr_group);
+	logger_mode_retval = sysfs_create_group(logger_mode_kobj, &logger_mode_attr_group);
 
-        if (logger_mode_retval)
+	if (logger_mode_retval)
 	{
-			kobject_put(logger_mode_kobj);
+		kobject_put(logger_mode_kobj);
 	}
 
 	// initialize logger mode to 1 (enabled) as default
 	logger_mode = 1;
 
-        return (logger_mode_retval);
+	return (logger_mode_retval);
 }
 
 
@@ -102,7 +94,6 @@ void logger_mode_exit(void)
 {
 	kobject_put(logger_mode_kobj);
 }
-
 
 /* define driver entry points */
 module_init(logger_mode_init);
