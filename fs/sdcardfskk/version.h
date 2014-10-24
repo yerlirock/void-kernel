ef0bdf102d51baadfcf1811f400cb790656437f4
arter97 <qkrwngud825@gmail.com>
2014-10-24 22:47:15 +0900

    fs: implement hybrid sdcardfs for Android 4.3 & 4.4
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- fs/sdcardfskk/version.h
+++ fs/sdcardfskk/version.h
@@ -1,16 +1,16 @@
 /*
- * The sdcardfs
+ * The sdcardfskk
  *
  * Copyright (c) 2013 Samsung Electronics Co. Ltd
  *   Authors: Daeho Jeong, Woojoong Lee, Kitae Lee, Yeongjin Gil
  *
  * Revision History
  * 2014.06.24 : Release Version 2.1.0
- *    - Add sdcardfs version
+ *    - Add sdcardfskk version
  *    - Add kernel log when put_super
  * 2014.07.21 : Release Version 2.1.1
- *    - Add sdcardfs_copy_inode_attr() to fix permission issue
- *    - Delete mmap_sem lock in sdcardfs_setattr() to avoid deadlock
+ *    - Add sdcardfskk_copy_inode_attr() to fix permission issue
+ *    - Delete mmap_sem lock in sdcardfskk_setattr() to avoid deadlock
  */
 
-#define SDCARDFS_VERSION "2.1.1"
+#define SDCARDFSKK_VERSION "2.1.1"
