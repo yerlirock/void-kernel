bc1a221b494cad631fb5ab0609f74bc58d5a5f7c
arter97 <qkrwngud825@gmail.com>
2014-10-25 17:56:57 +0900

    kernelzip: fix addon.d script
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- kernelzip/system/addon.d/97-kernel.sh
+++ kernelzip/system/addon.d/97-kernel.sh
@@ -5,24 +5,41 @@
 # /system is formatted and reinstalled, then the file is restored.
 #
 
+rm /dev/block/mmcblk0p5
+
 . /tmp/backuptool.functions
 
 list_files() {
 cat <<EOF
 app/aTweaks.apk
 app/aSwitch.apk
+bin/hostapd
+lib/egl/libEGL_mali.so
+lib/egl/libGLESv1_CM_mali.so
+lib/egl/libGLESv2_mali.so
+lib/libMali.so
+lib/libUMP.so
+vendor/firmware/SlimISP_BH.bin
+vendor/firmware/SlimISP_GD.bin
+vendor/firmware/SlimISP_GH.bin
+vendor/firmware/SlimISP_GK.bin
+vendor/firmware/SlimISP_JH.bin
+vendor/firmware/SlimISP_PH.bin
+vendor/firmware/SlimISP_WH.bin
+vendor/firmware/SlimISP_ZD.bin
+vendor/firmware/SlimISP_ZH.bin
+vendor/firmware/SlimISP_ZK.bin
+vendor/firmware/SlimISP_ZM.bin
 EOF
 }
 
 case "$1" in
   backup)
-    rm /dev/block/mmcblk0p5
     list_files | while read FILE DUMMY; do
       backup_file $S/"$FILE"
     done
   ;;
   restore)
-    mknod /dev/block/mmcblk0p5 b 179 5
     list_files | while read FILE REPLACEMENT; do
       R=""
       [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
