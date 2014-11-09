3141965bada2db5f6d2337177f745e6a574f1041
arter97 <qkrwngud825@gmail.com>
2014-11-09 20:46:23 +0900

    Update update_random.sh script
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- update_random.sh
+++ update_random.sh
@@ -11,4 +11,43 @@ sed -i -e 's/uapi\/linux\/random.h/linux\/uapi_random.h/g' include/linux/random.
 
 sed -i 's/[[:space:]]*$//' include/linux/uapi_random.h include/linux/random.h include/trace/events/random.h drivers/char/random.c lib/random32.c
 
+echo "--- a/drivers/char/random.c
++++ b/drivers/char/random.c
+@@ -1106,7 +1106,7 @@ static void extract_buf(struct entropy_store *r, __u8 *out)
+ 	__mix_pool_bytes(r, hash.w, sizeof(hash.w));
+ 	spin_unlock_irqrestore(&r->lock, flags);
+ 
+-	memzero_explicit(workspace, sizeof(workspace));
++	memset(workspace, 0, sizeof(workspace));
+ 
+ 	/*
+ 	 * In case the hash function has some recognizable output
+@@ -1118,7 +1118,7 @@ static void extract_buf(struct entropy_store *r, __u8 *out)
+ 	hash.w[2] ^= rol32(hash.w[2], 16);
+ 
+ 	memcpy(out, &hash, EXTRACT_SIZE);
+-	memzero_explicit(&hash, sizeof(hash));
++	memset(&hash, 0, sizeof(hash));
+ }
+ 
+ /*
+@@ -1175,7 +1175,7 @@ static ssize_t extract_entropy(struct entropy_store *r, void *buf,
+ 	}
+ 
+ 	/* Wipe data just returned from memory */
+-	memzero_explicit(tmp, sizeof(tmp));
++	memset(tmp, 0, sizeof(tmp));
+ 
+ 	return ret;
+ }
+@@ -1218,7 +1218,7 @@ static ssize_t extract_entropy_user(struct entropy_store *r, void __user *buf,
+ 	}
+ 
+ 	/* Wipe data just returned from memory */
+-	memzero_explicit(tmp, sizeof(tmp));
++	memset(tmp, 0, sizeof(tmp));
+ 
+ 	return ret;
+ }" | patch -p1
+
 make drivers/char/random.o lib/random32.o
