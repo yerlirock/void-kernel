13d5ddc6d61e8d895c1e612e21e22be159d01152
arter97 <qkrwngud825@gmail.com>
2014-11-16 20:12:11 +0900

    ramdisk: dump kernel panic log to local storage
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- ramdisk/res/dumper.sh
+++ ramdisk/res/dumper.sh
@@ -3,18 +3,8 @@
 export PATH=/res/asset:$PATH
 
 if cat /proc/last_kmsg | grep -q "Kernel panic"; then
-	time=0
-	until ping -c 1 8.8.8.8
-	do
-		sleep 1
-		if [[ $time == "180" ]]; then
-			return 1
-		fi
-		time=$(($time + 1))
-	done
-	cat /proc/last_kmsg > /dev/last_kmsg_tmp
-	ftpput -u ftp_odroid -p develoid arter97.iptime.org panic_$(date +%s).log /dev/last_kmsg_tmp
-	rm /dev/last_kmsg_tmp
+	mkdir -p /sdcard/arter97 2>/dev/null
+	cat /proc/last_kmsg > /sdcard/arter97/panic_$(date +%s).log
 	dmesg -c
 	sync
 fi
