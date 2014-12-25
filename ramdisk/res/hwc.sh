def4647f8e4d6b63a8ca408e2c680449343eb58e
arter97 <qkrwngud825@gmail.com>
2014-12-25 13:46:21 +0900

    Improve working HWC detection
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- ramdisk/res/hwc.sh
+++ ramdisk/res/hwc.sh
@@ -7,31 +7,26 @@ if [ -e /arter97/data ] ; then
 	DATA=/arter97/data
 fi
 
-echo "hw/gralloc.default.so
-hw/gralloc.exynos4.so
-hw/gralloc.smdk4x12.so
-hw/hwcomposer.exynos4.so
-hw/hwcomposer.smdk4x12.so
-libExynosHWCService.so
-libExynosIPService.so
-libfimg.so
-libhdmiclient.so
-libsecion.so
-libsync.so" | while read file; do
-	if [ ! -e /system/lib/$file ] ; then
-		mount -o rw,remount /system
-		touch /system/lib/$file
-	fi
-	if [ ! -e $DATA/arter97/"$@"/lib/$file ] ; then
-		touch $DATA/arter97/"$@"/lib/$file
-	fi
-	if [[ $(md5sum /system/lib/$file | awk '{print $1}') != $(md5sum $DATA/arter97/"$@"/lib/$file | awk '{print $1}') ]]; then
-		mount -o rw,remount /system
-		cp -p $DATA/arter97/"$@"/lib/$file /system/lib/$file
+HWC=0
+if strings /system/lib/hw/hwcomposer.smdk4x12.so | grep -q libExynosHWCService.so; then
+	HWC=1
+fi
+if strings /system/lib/hw/hwcomposer.exynos4.so | grep -q libExynosHWCService.so; then
+	HWC=1
+fi
+
+if [[ $HWC == "0" ]]; then
+	mount -o rw,remount /system
+	cd $DATA/arter97/"$@"/lib
+	find . -type f | while read file; do
+		rm /system/lib/$file
+		cp $file /system/lib/$file
 		chown 0.0 /system/lib/$file
 		chmod 644 /system/lib/$file
-	fi
+	done
+	cp -p /system/lib/gralloc.exynos4.so /system/lib/gralloc.smdk4x12.so
+	cp -p /system/lib/hwcomposer.exynos4.so /system/lib/hwcomposer.smdk4x12.so
 	if [ ! -e /arter97/data ] ; then
 		mount -o ro,remount /system
 	fi
-done
+fi
