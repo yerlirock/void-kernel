79db121f7f2452db9125ebec22c0826b2a2a7191
arter97 <qkrwngud825@gmail.com>
2015-01-09 21:57:36 +0900

    fs: rework f2fs
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -1225,6 +1225,8 @@ static int __init init_f2fs_fs(void)
 {
 	int err;
 
+	f2fs_build_trace_ios();
+
 	err = init_inodecache();
 	if (err)
 		goto fail;
@@ -1273,6 +1275,7 @@ static void __exit exit_f2fs_fs(void)
 	destroy_node_manager_caches();
 	destroy_inodecache();
 	kset_unregister(f2fs_kset);
+	f2fs_destroy_trace_ios();
 }
 
 module_init(init_f2fs_fs)
