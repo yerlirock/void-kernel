820770037c7dd5fe4d5570f3a7f8071c5a406d1e
arter97 <qkrwngud825@gmail.com>
2015-03-02 23:20:00 +0900

    ramdisk: fix preloadswap failing due to disappeared nod
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- ramdisk/res/preloadswap.sh
+++ ramdisk/res/preloadswap.sh
@@ -8,6 +8,7 @@ fi
 
 if [[ $(cat /data/.arter97/preloadswap) == "1" ]]; then
 	rm /sbin/sswap
-	mkswap /dev/block/mmcblk0p10
-	swapon -p 5 /dev/block/mmcblk0p10
+	mv /dev/block/mmcblk0p10 /
+	mkswap /mmcblk0p10
+	swapon -p 5 /mmcblk0p10
 fi
