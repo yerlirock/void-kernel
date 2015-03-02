e47392fcf8b8b9413156ed6a74437250c6f9c38d
arter97 <qkrwngud825@gmail.com>
2015-03-02 22:58:57 +0900

    ramdisk: improve vnswap working method
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- ramdisk/res/vnswap.sh
+++ ramdisk/res/vnswap.sh
@@ -7,11 +7,5 @@ if [[ $@ == "1" ]]; then
 fi
 
 if [[ $(cat /data/.arter97/vnswap) == "1" ]]; then
-	echo "
-# SSWAP
-service swapon /sbin/sswap -s
-    class core
-    user root
-    group root
-    oneshot" >> /init.smdk4x12.rc
+	/sbin/sswap -s
 fi
