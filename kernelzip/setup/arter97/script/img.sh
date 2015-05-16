4152485a44ef7dd309616df24064c212436b1a42
arter97 <qkrwngud825@gmail.com>
2015-05-16 23:42:42 +0900

    kernelzip: make sure nod is there
    After ROM is flashed with addon/97-kernel.sh, /dev/block/mmcblk0p5 gets removed.
    So let's make sure it's re-made at the kernel installation.
    
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- kernelzip/setup/arter97/script/img.sh
+++ kernelzip/setup/arter97/script/img.sh
@@ -8,4 +8,5 @@ cd /tmp/
 /tmp/arter97/busybox touch /data/media/0/.arter97/shared
 /tmp/arter97/busybox chown media_rw:media_rw /data/media/0/.arter97/shared
 /tmp/arter97/busybox chmod 777 /data/media/0/.arter97/shared
+/tmp/arter97/busybox mknod /dev/block/mmcblk0p5 b 179 5
 /tmp/arter97/busybox unxz -c /tmp/img.tar.xz | /tmp/arter97/busybox tar -xOf - "$@" | /tmp/arter97/busybox dd of=/dev/block/mmcblk0p5
