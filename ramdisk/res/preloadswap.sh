39f16e99a18052975ed868a51333f91cd908a297
Park Ju Hyung <qkrwngud825@gmail.com>
2015-08-01 03:45:42 +0900

    ramdisk: try discarding on preloadswap load
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- ramdisk/res/preloadswap.sh
+++ ramdisk/res/preloadswap.sh
@@ -4,5 +4,5 @@ export PATH=/res/asset:$PATH
 
 if [[ $(cat /data/.arter97/preloadswap) == "1" ]]; then
 	mkswap /dev/block/mmcblk0p10
-	swapon -p 5 /dev/block/mmcblk0p10
+	swapon -d[once] -p 5 /dev/block/mmcblk0p10
 fi
