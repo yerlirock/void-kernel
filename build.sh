64ab96fe8402c05e3684de4431b328d1268b62c6
Alex Raschi <raschi.alex@gmail.com>
2016-03-25 13:33:03 +0100

    release2

--- build.sh
+++ build.sh
@@ -2,15 +2,14 @@
 # build.sh -> build/rebuild kernel
 # build.sh zip -> create/recreate zip only
 
-VERSION="release1-$(git rev-parse --short HEAD)"
+ZIP="void-kernel-release2-$(git rev-parse --short HEAD)"
+EXCLUDE=".gitignore modules/placeholder"
 
 if [ ! "$1" = "zip" ]; then
 	make ARCH=arm n7100_defconfig
-	make -j16 ARCH="arm" CROSS_COMPILE="../gcc-arm-none-eabi-4_9-2015q3/bin/arm-none-eabi-"
+	make -j16 ARCH=arm CROSS_COMPILE="../gcc-arm-none-eabi-4_9-2015q3/bin/arm-none-eabi-"
 fi;
 cp arch/arm/boot/zImage template/zImage
-find . -name "*.ko" -exec cp {} ./template/modules \;
+find . -name "*.ko" -exec cp {} template/modules \;
 cd template
-find . -name placeholder -exec rm -rf {} \;
-7z a -mx9 stock-kernel-"$VERSION"-n7100.zip *
-mv stock-kernel-"$VERSION"-n7100.zip ..
+zip -r9 ../$ZIP * -x $EXCLUDE
