28d50d905b4ac66803b8533e54f6855b52a39088
Alex Raschi <raschi.alex@gmail.com>
2016-03-26 14:07:14 +0100

    build.sh: add .config option

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 # build.sh -> build/rebuild kernel
 # build.sh zip -> create/recreate zip only
+# build.sh .config -> build/rebuild kernel using .config
 
 ZIP="void-kernel-release2-$(git rev-parse --short HEAD)-n7100"
 EXCLUDE=".gitignore modules/placeholder"
 
 if [ ! "$1" = "zip" ]; then
-	make ARCH=arm n7100_defconfig
+	if [ ! "$1" = ".config" ]; then
+		make ARCH=arm n7100_defconfig
+	fi
 	make -j16 ARCH=arm CROSS_COMPILE="../gcc-arm-none-eabi-4_9-2015q3/bin/arm-none-eabi-"
-fi;
+fi
 cp arch/arm/boot/zImage template/zImage
 find . -name "*.ko" -exec cp {} template/modules \;
 cd template
