7dbb1c4f854968cb5490dac30c8c51a4942fd103
Alex Raschi <raschi.alex@gmail.com>
2016-03-26 23:26:45 +0100

    touchwake: fix knockon for note 2 (experimental)

--- drivers/input/touchscreen/mms152_ts.c
+++ drivers/input/touchscreen/mms152_ts.c
@@ -1099,7 +1099,7 @@ static irqreturn_t mms_ts_interrupt(int irq, void *dev_id)
 #ifdef CONFIG_TOUCH_WAKE
 		if (mms_ts_suspended) {
 			if (knockon) {
-				if (touch_is_pressed == 0) {
+				if (touch_is_pressed == 1) {
 					if (knockon_reset) {
 						knockon_reset = false;
 						touch_press();
