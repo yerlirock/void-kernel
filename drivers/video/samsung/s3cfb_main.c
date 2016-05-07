9aa8e2037e0f1ce3a58d4cf5cdcf0fb2d96e9d59
Alex Raschi <raschi.alex@gmail.com>
2016-05-07 22:00:10 +0200

    Revert "s3cfb: make it compatible with Samsung stock ROM"
    This reverts commit deb743e21ac9948b97d49de7202e72fe31c687a1.

--- drivers/video/samsung/s3cfb_main.c
+++ drivers/video/samsung/s3cfb_main.c
@@ -394,31 +394,18 @@ static ssize_t fimd_dump_show(struct device *dev,
 }
 static DEVICE_ATTR(fimd_dump, 0444, fimd_dump_show, NULL);
 
-static ssize_t s3c_fb_vsync_time(struct device *dev,
-                struct device_attribute *attr, char *buf)
-{
-	struct s3cfb_global *fbdev[1];
-	fbdev[0] = fbfimd->fbdev[0];
-
-    	return snprintf(buf, PAGE_SIZE, "%llu",
-			((fbdev[0] != 0) ?
-			ktime_to_ns(fbdev[0]->vsync_info.timestamp) : 0));
-}
-
-static DEVICE_ATTR(vsync_time, S_IRUGO, s3c_fb_vsync_time, NULL);
-
 static ssize_t vsync_event_show(struct device *dev,
 	struct device_attribute *attr, char *buf)
 {
 	struct s3cfb_global *fbdev[1];
 	fbdev[0] = fbfimd->fbdev[0];
 
-	return snprintf(buf, PAGE_SIZE, "VSYNC=%llu",
+	return snprintf(buf, PAGE_SIZE, "%llu",
 			((fbdev[0] != 0) ?
 			ktime_to_ns(fbdev[0]->vsync_info.timestamp) : 0));
 }
 
-static DEVICE_ATTR(vsync_event, 0444, vsync_event_show, NULL);
+static DEVICE_ATTR(vsync_time, S_IRUGO, vsync_event_show, NULL);
 
 #if defined(CONFIG_FB_S5P_VSYNC_THREAD)
 static int s3cfb_wait_for_vsync_thread(void *data)
@@ -435,14 +422,8 @@ static int s3cfb_wait_for_vsync_thread(void *data)
 				fbdev->vsync_info.timestamp) &&
 				fbdev->vsync_info.active);
 
-		SAMSUNGROM {
-		sysfs_notify(&fbdev->fb[pdata->default_win]->dev->kobj,
-				NULL, "vsync_event");
-		}
-
-		AOSPROM {
-                sysfs_notify(&fbdev->dev->kobj, NULL, "vsync_time");
-		}
+		sysfs_notify(&fbdev->dev->kobj,
+				NULL, "vsync_time");
 	}
 
 	return 0;
@@ -1317,13 +1298,8 @@ static int s3cfb_probe(struct platform_device *pdev)
 		if (ret < 0)
 			dev_err(fbdev[0]->dev, "failed to add sysfs entries\n");
 
-		ret = device_create_file(fbdev[i]->fb[pdata->default_win]->dev,
-					&dev_attr_vsync_event);
-		if (ret < 0)
-			dev_err(fbdev[0]->dev, "failed to add sysfs entries\n");
-
-		ret = device_create_file(fbdev[i]->dev, &dev_attr_vsync_time);
-
+		ret = device_create_file(fbdev[i]->dev,
+					&dev_attr_vsync_time);
 		if (ret < 0)
 			dev_err(fbdev[0]->dev, "failed to add sysfs entries\n");
 
