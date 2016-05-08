7f853e7ff88511b1a2ff9518c55da99e6b3893d0
Alex Raschi <raschi.alex@gmail.com>
2016-05-08 10:56:55 +0200

    f2fs: fix build with CONFIG_SDCARD_FS disabled

--- fs/f2fs/inline.c
+++ fs/f2fs/inline.c
@@ -286,8 +286,12 @@ process_inline:
 }
 
 struct f2fs_dir_entry *find_in_inline_dir(struct inode *dir,
+#ifdef CONFIG_SDCARD_FS
 			struct f2fs_filename *fname, struct page **res_page,
 			unsigned int flags)
+#else
+			struct f2fs_filename *fname, struct page **res_page)
+#endif
 {
 	struct f2fs_sb_info *sbi = F2FS_SB(dir->i_sb);
 	struct f2fs_inline_dentry *inline_dentry;
@@ -306,7 +310,11 @@ struct f2fs_dir_entry *find_in_inline_dir(struct inode *dir,
 	inline_dentry = inline_data_addr(ipage);
 
 	make_dentry_ptr(NULL, &d, (void *)inline_dentry, 2);
+#ifdef CONFIG_SDCARD_FS
 	de = find_target_dentry(fname, namehash, NULL, &d, flags);
+#else
+	de = find_target_dentry(fname, namehash, NULL, &d);
+#endif
 	unlock_page(ipage);
 	if (de)
 		*res_page = ipage;
