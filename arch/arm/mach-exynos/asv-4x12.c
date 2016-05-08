1e6105c5253426c058f0434113668d01d75a4cd7
Alex Raschi <raschi.alex@gmail.com>
2016-05-08 16:44:45 +0200

    mali400: cleanup

--- arch/arm/mach-exynos/asv-4x12.c
+++ arch/arm/mach-exynos/asv-4x12.c
@@ -21,7 +21,7 @@
 #include <mach/map.h>
 #include <plat/cpu.h>
 
-#ifdef CONFIG_GPU_CLOCK_CONTROL
+#ifdef CONFIG_GPU_CONTROL
 typedef unsigned long mali_bool;
 mali_bool mali_dvfs_table_update(void);
 #endif
@@ -262,7 +262,7 @@ static int exynos4x12_asv_store_result(struct samsung_asv *asv_info)
 
 	exynos4x12_pre_set_abb();
 
-#ifdef CONFIG_GPU_CLOCK_CONTROL
+#ifdef CONFIG_GPU_CONTROL
 	mali_dvfs_table_update();
 #endif
 
@@ -363,7 +363,7 @@ int exynos4x12_asv_init(struct samsung_asv *asv_info)
 #ifdef CONFIG_ABB_CONTROL
 		abb_control_init();
 #endif
-#ifdef CONFIG_GPU_CLOCK_CONTROL
+#ifdef CONFIG_GPU_CONTROL
 		mali_dvfs_table_update();
 #endif
 
