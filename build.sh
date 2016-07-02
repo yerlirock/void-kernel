89e157779119201cdf2300a4ba9ed1a55a3061fc
Alex Raschi <raschi.alex@gmail.com>
2016-07-02 14:30:10 +0200

    linux: add automated build system

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Simple script to automate building process (nothing special!)
+#
+# build.sh -> build/rebuild kernel
+# build.sh zip -> create/recreate zip only
+# build.sh .config -> build/rebuild kernel using .config
+
+if [[ ! "$1" = "zip" ]]; then
+	if [[ ! "$1" = ".config" ]]; then
+		make n7100_defconfig; fi
+	make -j8
+fi
+
+cp arch/arm/boot/zImage template/zImage
+find . -name "*.ko" -exec cp {} template/modules \;
+cd template
+zip -r9 ../"arter97-kernel-$(cat ../version)-g$(git rev-parse --short HEAD)-n7100.zip" * -x ".gitignore" "modules/placeholder"
